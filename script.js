fac183c271d08026c1b4fa213bd9c0e88c3adfd2
Mojtaba <mojtabasultani82@gmail.com>
2024-11-15 06:30:50 -0800

    final project

--- script.js
+++ script.js
@@ -2,4 +2,11 @@ document.getElementById('custom-file').addEventListener('change', function(event
     const fileName = event.target.files[0] ? event.target.files[0].name : 'انتخاب فایل';
     document.querySelector('.custom-file-label').textContent = fileName;
   });
-  
+  document.getElementById('custom-file1').addEventListener('change', function(event) {
+    const fileName = event.target.files[0] ? event.target.files[0].name : 'انتخاب فایل';
+    document.querySelector('.custom-file-label1').textContent = fileName;
+  });
+  function updateOutput(input, outputId) {
+    const output = document.getElementById(outputId);
+    output.value = input.value;
+}
