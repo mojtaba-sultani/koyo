2c357f1cbe4223db3bade4098c8c20ee513249ec
Mojtaba <mojtabasultani82@gmail.com>
2024-11-30 09:48:38 -0800

    form style

--- script/script.js
+++ script/script.js
@@ -1,26 +1,26 @@
-  function updateOutput(input, outputId) {
-    const output = document.getElementById(outputId);
-    output.value = input.value;
+function updateOutput(input, outputId) {
+  const output = document.getElementById(outputId);
+  output.value = input.value;
 }
 
 document.addEventListener('DOMContentLoaded', function() {
 
-  const navLinks = document.querySelectorAll('.nav-item');
-  const divs = document.querySelectorAll("nav li div"); // انتخاب تمام div ها
+const navLinks = document.querySelectorAll('.nav-item');
+const divs = document.querySelectorAll("nav li a");
   const currentPath = window.location.pathname.split('/').pop();
+
+navLinks.forEach(function(navLink) {
+  const link = navLink.querySelector('a');
+  const linkPath = link.getAttribute('href').split('/').pop();
   
-  navLinks.forEach(function(navLink) {
-    const link = navLink.querySelector('a');
-    const linkPath = link.getAttribute('href').split('/').pop();
-    
-    if (linkPath === currentPath) {
-      navLink.classList.add('active');
-      const div = navLink.querySelector('div');
-      if (div) {
-        div.classList.add('bg-warning');
-      }
-    } else {
-      navLink.classList.remove('active');
+  if (linkPath === currentPath) {
+    navLink.classList.add('active');
+    const a = navLink.querySelector('a');
+    if (a) {
+      a.classList.add('bg-warning');
     }
-  });
+  } else {
+    navLink.classList.remove('active');
+  }
+});
 });
